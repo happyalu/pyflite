a7b9aa90bc836fb53f6d8fbaff23ea4d514bdba1
Kevin Yang <skyang@gmail.com>
2016-10-22 16:02:34 -0400

    Give setup the ability to take the location of includes as an environment variable.
    Could not get this to work using argparser, because it kept trying to pass args to setup, but the env variables work.

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from distutils.core import setup, Extension
 
+import os
+print os.environ['FLITE_LIBS']
+print os.environ['FLITE_INC']
+
+inc_dirs=['flite/include']
+lib_dirs=['flite/build/x86_64-linux-gnu/lib']
+
+if os.environ['FLITE_INC']:
+    inc_dirs = [os.environ['FLITE_INC'],]
+if os.environ['FLITE_LIBS']:
+    lib_dirs = [os.environ['FLITE_LIBS'],]
+
+
 setup(name='pyflite',
       version='0.1',
       ext_modules=[Extension('_pyflite', ['pyflite.i', 'pyflite.c'],
-                             include_dirs=['flite/include'],
-                             library_dirs=['flite/build/x86_64-linux-gnu/lib'],
+                             include_dirs=inc_dirs,
+                             library_dirs=lib_dirs,
                              libraries=['flite_cmulex', 'flite_usenglish', 'flite'],
                              swig_opts=['-modern', '-Iflite/include'])],
       py_modules=['pyflite']
